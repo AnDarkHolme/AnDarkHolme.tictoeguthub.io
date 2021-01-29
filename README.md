ac3efb7c0dffc9009c2ec7bf7d0062aa34e79c32
DESKTOP-OMR4PCG\anton <antoniys007@gmail.com>
2021-01-30 00:50:28 +0700

    СОздал папку для tic-tac-toe

--- README.md
+++
@@ -1,1 +0,0 @@
-# AnDarkHolme.tictoeguthub.io
--- script.js
+++
@@ -1,5 +0,0 @@
-function fun1(){
-    sel = document.getElementById('mySelect').selectedIndex;
-    options = document.getElementById('mySelect').options;
-    alert('Выбрана опция' + options[sel].text + typeof(sel) + typeof(options));
-}
