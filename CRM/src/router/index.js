3a03a9129aeff953fb4149cae0816bee7b3a7b0d
DESKTOP-OMR4PCG\anton <antoniys007@gmail.com>
2021-02-26 02:32:59 +0700

    Урок 2

--- CRM/src/router/index.js
+++ CRM/src/router/index.js
@@ -4,11 +4,60 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+const routes = [  
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register.vue')
+  },
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    meta: {layout: 'main'},
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail/:id',
+    name: 'detail',
+    meta: {layout: 'main'},
+    component: () => import('../views/Detail.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue')
   }
 ]
 
